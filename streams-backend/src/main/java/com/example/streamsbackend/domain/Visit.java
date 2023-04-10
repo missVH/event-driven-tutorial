f8d7f5eaac220283a53eb5ed4c0b3777f74f3a5a
Lisa <lisa.vanhooydonk@student.kdg.be>
2023-04-11 00:51:18 +0200

    added visit class

---
+++ streams-backend/src/main/java/com/example/streamsbackend/domain/Visit.java
@@ -0,0 +1,63 @@
+package com.example.streamsbackend.domain;
+
+public class Visit {
+  private String username;
+
+  private int location;
+
+  private String timeStart;
+
+  private String timeEnd;
+
+  public Visit() {
+  }
+
+  public Visit(String username, int location, String startVisit, String endVisit) {
+    this.username = username;
+    this.location = location;
+    this.timeStart = startVisit;
+    this.timeEnd = endVisit;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public int getLocation() {
+    return location;
+  }
+
+  public void setLocation(int location) {
+    this.location = location;
+  }
+
+  public String getTimeStart() {
+    return timeStart;
+  }
+
+  public void setTimeStart(String timeStart) {
+    this.timeStart = timeStart;
+  }
+
+  public String getTimeEnd() {
+    return timeEnd;
+  }
+
+  public void setTimeEnd(String timeEnd) {
+    this.timeEnd = timeEnd;
+  }
+
+  @Override
+  public String toString() {
+    return "Visit{" +
+        "username='" + username + '\'' +
+        ", location=" + location +
+        ", timeStart=" + timeStart +
+        ", timeEnd=" + timeEnd +
+        '}';
+  }
+}
